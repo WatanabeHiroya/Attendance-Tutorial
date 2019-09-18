ad2e8f0fdb5a63aaae14ad5b1bfd64394ae56fab
Hiroya <plastic.gu@gmail.com>
2019-09-18 09:23:15 +0000

    8.2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,12 +5,14 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+
   # 永続的セッションを記憶します（Userモデルを参照）
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
+
 
   # 永続的セッションを破棄します
   def forget(user)
@@ -19,12 +21,14 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
   
+  
   # セッションと@current_userを破棄します
   def log_out
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
+
 
   # 一時的セッションにいるユーザーを返します。
   # それ以外の場合はcookiesに対応するユーザーを返します。
@@ -39,6 +43,13 @@ module SessionsHelper
       end
     end
   end
+
+
+  # 渡されたユーザーがログイン済みのユーザーであればtrueを返します。
+  def current_user?(user)
+    user == current_user
+  end
+
 
   # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
   def logged_in?
