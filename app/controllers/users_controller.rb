95cbfdb3e6103223283cb24a0876c4a5f6d56d17
Hiroya <plastic.gu@gmail.com>
2019-12-21 06:39:35 +0000

    勤怠情報編集画面編集中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -68,7 +68,7 @@ class UsersController < ApplicationController
     end
 
     def basic_info_params
-      params.require(:user).permit(:department, :basic_time, :work_time)
+      params.require(:user).permit(:basic_time, :work_time)
     end
 
 
